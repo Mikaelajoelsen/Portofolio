0fee25fa0cd8736d66f0fade0f03c21311607ac7
Mikaelajoelsen <111288566+Mikaelajoelsen@users.noreply.github.com>
2024-07-17 20:53:32 +0200

    added last changes

--- src/components/socialmedia/index.jsx
+++ src/components/socialmedia/index.jsx
@@ -3,49 +3,40 @@ import { MdOutlineMail } from "react-icons/md";
 
 const SocialMediaProfiles = () => {
   return (
-    <div className="flex flex-col justify-center items-center m-24">
-      <h1 className="text-white font-extrabold text-3xl">
+    <div className="flex flex-col items-center justify-center m-24">
+      <h1 className="text-3xl font-extrabold text-white">
         Let´s talk about your next project!
       </h1>
-      <p className="text-gray-400 text-center">
+      <p className="text-center text-gray-400">
         If you are interested in working with me, send me a message!
       </p>
-      <div className="flex flex-col gap-5 w-96 justify-center mt-5">
+      <div className="flex flex-col justify-center gap-5 mt-5 w-96">
         <div className="flex justify-center gap-2">
-          <MdOutlineMail className="text-white mt-1" />
+          <MdOutlineMail className="mt-1 text-white" />
           <a href="mailto:agnesmik@hotmail.com" className="text-white">
             Send me an email
           </a>
         </div>
-        <div className="flex justify-center text-white gap-2">
+        <div className="flex justify-center gap-2 text-white">
           <a
             href="https://github.com/Mikaelajoelsen"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <FaGithub className="text-white mt-1" />
+            <FaGithub className="mt-1 text-white" />
           </a>
           Check out my Github profile
         </div>
-        <div className="flex justify-center text-white gap-2">
+        <div className="flex justify-center gap-2 text-white">
           <a
             href="https://www.linkedin.com/in/mikaela-joelsen-181766278/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <FaLinkedin className="text-white mt-1" />
+            <FaLinkedin className="mt-1 text-white mb-36" />
           </a>
           Check out my Linkedin Profile
         </div>
-      </div>
-      <div className="flex justify-center border border-2-solid-white mt-16 rounded-md w-36 h-7 mb-48">
-        <a
-          href="/public/Agnes Mikaela  Joelsen CV FrontEnd Utvikler.pdf"
-          download
-          className="text-white"
-        >
-          Download CV
-        </a>
       </div>
     </div>
   );
