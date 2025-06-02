b1d58f9dd188cb1f0134746188ff8d347e619d2e
Mikaelajoelsen <111288566+Mikaelajoelsen@users.noreply.github.com>
2025-06-02 20:27:10 +0200

    la til kort

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -2,36 +2,47 @@ import "./card.css";
 
 function Box() {
   return (
-    <div className="container flex flex-row justify-center flex-wrap gap-10">
+    <div className="container flex flex-row flex-wrap justify-center gap-10">
       <div className="box">
         <span></span>
-        <div className="content flex flex-col justify-center">
+        <div className="flex flex-col justify-center content">
+          <a href="/mywork">
+            <h2>ART PORTOFOLIO</h2>
+          </a>
+          <p className="flex justify-center w-40 m-5 font-thin">
+            Art portofolio for a recent client
+          </p>
+        </div>
+      </div>
+      <div className="box">
+        <span></span>
+        <div className="flex flex-col justify-center content">
           <a href="/mywork">
             <h2>HOLLIDAZE</h2>
           </a>
-          <p className="flex justify-center m-5 w-40 font-thin">
+          <p className="flex justify-center w-40 m-5 font-thin">
             Book your next dream location
           </p>
         </div>
       </div>
       <div className="box">
         <span></span>
-        <div className="content flex flex-col justify-center">
+        <div className="flex flex-col justify-center content">
           <a href="/mywork">
             <h2>AUCTION SITE</h2>
           </a>
-          <p className="flex justify-center m-5 w-36 font-thin">
+          <p className="flex justify-center m-5 font-thin w-36">
             Place a bid and find your next treasure
           </p>
         </div>
       </div>
       <div className="box">
         <span></span>
-        <div className="content flex flex-col justify-center">
+        <div className="flex flex-col justify-center content">
           <a href="/mywork">
             <h2>ECOMMERCE</h2>
           </a>
-          <p className="flex justify-center m-5 w-36 font-thin">
+          <p className="flex justify-center m-5 font-thin w-36">
             Shop Your Favourite products
           </p>
         </div>
